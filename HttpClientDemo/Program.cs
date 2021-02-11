d69487f53406a47a4f99bb25370234e2fe00e50a
borislavatblagoev <borislavatblagoev@gmail.com>
2021-02-11 16:53:07 +0200

    Make server to process Async operation.

--- HttpClientDemo/Program.cs
+++ HttpClientDemo/Program.cs
@@ -5,83 +5,90 @@
     using System.Net;
     using System.Net.Sockets;
     using System.Collections.Generic;
+    using System.Threading.Tasks;
 
     public class Program
     {
+        const string NewLine = "\r\n";
+
         public static void Main()
         {
             Console.OutputEncoding = Encoding.UTF8;
-            var listOfTweets = new List<string>();
-            const string NewLine = "\r\n";
             TcpListener tcpListener = new TcpListener(IPAddress.Loopback, 80);
             tcpListener.Start();
-
+            var listOfTweets = new List<string>();
             while (true)
             {
                 var client = tcpListener.AcceptTcpClient();
-                var sb = new StringBuilder();
+                ProcessClientAsync(client, listOfTweets);
+            }
+        }
 
-                using (var stream = client.GetStream())
+        private static async Task ProcessClientAsync(TcpClient client, List<string> listOfTweets)
+        {
+           
+            var sb = new StringBuilder();
+
+            using (var stream = client.GetStream())
+            {
+                var buffer = new byte[1000000];
+                var request = await stream.ReadAsync(buffer, 0, buffer.Length);
+                var requestString = Encoding.UTF8.GetString(buffer, 0, request);
+                string response = string.Empty;
+                sb.Append(requestString);
+
+                if (requestString[0] == 'G')
                 {
-                    var buffer = new byte[1000000];
-                    var request = stream.Read(buffer, 0, buffer.Length);
-                    var requestString = Encoding.UTF8.GetString(buffer, 0, request);
-                    string response = string.Empty;
-                    sb.Append(requestString);
+                    string html = $"<h1>Hello from Boro's Server</h1>" +
+                                  $"<h2>Tweet</h2>" +
+                                  $"<form action=/tweet method=post><input name=tweet />" +
+                                  $"<p></p>" +
+                                  $"<input type=submit value=Tweet /></form>";
 
-                    if (requestString[0] == 'G')
+                    response = "HTTP/1.1 200 OK" + NewLine +
+                       "Server: BoroServer 2020" + NewLine +
+                       "Content-Type: text/html; charset=utf-8" + NewLine +
+                       "Content-Lenght: " + html.Length + NewLine +
+                       NewLine +
+                       html + NewLine;
+                }
+                else if (requestString[0] == 'P')
+                {
+                    var position = 0;
+
+                    for (int i = 0; i < requestString.Length; i++)
                     {
-                        string html = $"<h1>Hello from Boro's Server</h1>" +
-                                      $"<h2>Tweet</h2>" +
-                                      $"<form action=/tweet method=post><input name=tweet />" +
-                                      $"<p></p>" +
-                                      $"<input type=submit value=Tweet /></form>";
-
-                         response = "HTTP/1.1 200 OK" + NewLine +
-                            "Server: BoroServer 2020" + NewLine +
-                            "Content-Type: text/html; charset=utf-8" + NewLine +
-                            "Content-Lenght: " + html.Length + NewLine +
-                            NewLine +
-                            html + NewLine;
-                    }
-                    else if (requestString[0] == 'P')
-                    {
-                        var position = 0;
-
-                        for (int i = 0; i < requestString.Length; i++)
+                        if (requestString[i] == '=')
                         {
-                            if (requestString[i] == '=')
-                            {
-                                position = i;
-                            }
+                            position = i;
                         }
-
-                        var tweet = requestString.Substring(position + 1);
-                        tweet = tweet.Replace('+', ' ');
-                        listOfTweets.Add($"<h2>{tweet} <span>{DateTime.Now}</span></h2>");
-
-                        string html = $"<h1>Tweet</h1>" +
-                                      $"<form action=/tweet method=post><input name=tweet />" +
-                                      $"<p></p>" +
-                                      $"<input type=submit value=Tweet /></form>" +
-                                      $"<p></p>" +
-                                      $"{string.Join(NewLine, listOfTweets)}";
-
-                        response = "HTTP/1.1 200 OK" + NewLine +
-                           "Server: BoroServer 2020" + NewLine +
-                           "Content-Type: text/html; charset=utf-8" + NewLine +
-                           "Content-Lenght: " + html.Length + NewLine +
-                           NewLine +
-                           html + NewLine;
                     }
 
-                    var responseByte = Encoding.UTF8.GetBytes(response);
-                    stream.Write(responseByte, 0, responseByte.Length);
+                    var tweet = requestString.Substring(position + 1);
+                    tweet = tweet.Replace('+', ' ');
+                    listOfTweets.Add($"<h2>{tweet} <span>{DateTime.Now}</span></h2>");
+
+                    string html = $"<h1>Tweet</h1>" +
+                                  $"<form action=/tweet method=post><input name=tweet />" +
+                                  $"<p></p>" +
+                                  $"<input type=submit value=Tweet /></form>" +
+                                  $"<p></p>" +
+                                  $"{string.Join(NewLine, listOfTweets)}";
+
+                    response = "HTTP/1.1 200 OK" + NewLine +
+                       "Server: BoroServer 2020" + NewLine +
+                       "Content-Type: text/html; charset=utf-8" + NewLine +
+                       "Content-Lenght: " + html.Length + NewLine +
+                       NewLine +
+                       html + NewLine;
                 }
 
-                Console.WriteLine(sb.ToString());
-                Console.WriteLine(new string('=', 70));
+                var responseByte = Encoding.UTF8.GetBytes(response);
+                await stream.WriteAsync(responseByte, 0, responseByte.Length);
             }
+
+            Console.WriteLine(sb.ToString());
+            Console.WriteLine(new string('=', 70));
         }
     }
 }
